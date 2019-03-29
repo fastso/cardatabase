873e64057afb367d50e1164ca2941f04fc15be89
lihui <fastso.biko@gmail.com>
2019-03-29 15:51:53 +0900

    commit

--- src/test/java/com/fastso/cardatabase/CardatabaseApplicationTests.java
+++ src/test/java/com/fastso/cardatabase/CardatabaseApplicationTests.java
@@ -1,16 +1,23 @@
 package com.fastso.cardatabase;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.fastso.cardatabase.web.CarController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CardatabaseApplicationTests {
+    @Autowired
+    private CarController controller;
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+        assertThat(controller).isNotNull();
+    }
 
 }
